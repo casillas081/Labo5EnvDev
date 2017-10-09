b4968dce79d258bccd2d67ca94ec3d7fcad24511
casillas081 <christ.schepmans@hotmail.com>
2017-10-09 10:19:19 +0200

    Labo5Fin

---
+++ Labo5/Labo5/Converter/WeatherDescriptionToImageValueConverter.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Labo5.Converter
+{
+    class WeatherDescriptionToImageValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var forecast = (string)value;
+            if (forecast.Contains("légères pluies"))
+                return new BitmapImage(new Uri("ms-appx:/Images/nuageux.png"));
+            else
+                return new BitmapImage(new Uri("ms-appx:/Images/ensoleille.png"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Labo5/Labo5/DAO/WeatherService.cs
@@ -0,0 +1,30 @@
+﻿using Labo5.Model;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo5.DAO
+{
+    class WeatherService
+    {
+        public async Task<IEnumerable<WeatherForecast>> GetForecast()
+        {
+            var wc = new HttpClient();
+            var weather = await wc.GetStringAsync(new Uri("http://api.openweathermap.org/data/2.5/forecast?q=Namur&lang=fr&appid=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
+            var rawWeather = JObject.Parse(weather);
+            var forecast = rawWeather["list"].Children().Select(d => new WeatherForecast()
+            {
+                Date = d["dt_txt"].Value<DateTime>(),
+                MinTemp = d["main"]["temp_min"].Value<double>(),
+                MaxTemp = d["main"]["temp_max"].Value<double>(),
+                WeatherDescription = d["weather"].First["description"].Value<string>(),
+                WindSpeed = d["wind"]["speed"].Value<double>()
+            });
+            return forecast;
+        }
+    }
+}
